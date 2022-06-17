b09dc22cd69e08a407b0f1e33eac069d256a6665
xiaozhangpeng <1095338869@qq.com>
2022-06-17 08:29:13 +0800

    测试Github

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="app">
+
+<!--        keep-alive可以使得组件不被销毁-->
+        <keep-alive exclude="Detail">
+            <router-view></router-view>
+        </keep-alive>
+
+
+        <MainTabBar></MainTabBar>
+    </div>
+</template>
+
+<script>
+
+    //引如MainTabBar
+    import MainTabBar from "./components/content/mainTabbar/MainTabBar"
+
+    export default {
+        name: 'app',
+        components: {
+            MainTabBar
+        }
+    }
+</script>
+
+<style>
+    /*<!--  引入base.css-->*/
+    @import "./assets/css/base.css";
+
+
+</style>
